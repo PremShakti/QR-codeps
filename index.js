f5be079b73c8b409947dbaf2d79a74d6483459fa
PremShakti <premshakti.ps@gmail.com>
2023-06-02 14:06:06 +0530

    second

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const qr = require('qr-image');
+const app = express();
+app.use(express.json())
+require("dotenv").config()
+//this app is created by prem chandr adas
+const PORT=process.env.PORT || 3000
+app.get("/qrcode", (req, res) => {
+    const text = req.query.text || 'Hello, World!';
+
+    try {
+        const qrCodeUrl = `data:image/png;base64,${qr.imageSync(text, { type: 'png' }).toString('base64')}`;
+        res.status(200).send({ "url": qrCodeUrl })
+
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+
+})
+
+app.listen(PORT, () => {
+    try {
+        console.log(`server is running on PORT ${PORT}`)
+    } catch (error) {
+     console.log(error.message)
+    }
+})
